b20f0879554dea357c24b8ada17a9dc553feb3cf
Philippe Proulx <eeppeliteloop@gmail.com>
2019-04-03 04:21:56 -0400

    Add dedicated command to generate names from the UDA directory
    This patch adds a new command, `qng-uda`, which is the same as `qng`,
    but its sources are names extracted from the UDA member directory.
    
    You can choose the gender with --gender just like with `qng`.
    
    You can choose the membership category with --type (actor/actress, host,
    or singer).
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ qng/common.py
@@ -0,0 +1,46 @@
+import argparse
+import json
+import operator
+import os
+import random
+import sys
+import unicodedata
+from typing import Optional
+
+import qng
+
+
+_BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+_DATA_DIR = os.path.join(_BASE_DIR, 'data')
+
+
+def _strip_diacritics(string: str) -> str:
+    return (
+        unicodedata.normalize('NFKD', string)
+        .encode('ascii', 'ignore')
+        .decode('utf-8')
+    )
+
+
+def _snakify_name(name: str) -> str:
+    name = _strip_diacritics(name)
+    name = name.lower()
+    name = name.replace(' ', '-')
+
+    return name
+
+
+def _format_name(name: str, surname: str, snake_case: bool = False) -> str:
+    if snake_case:
+        name = _snakify_name(name)
+        surname = _snakify_name(surname)
+        full_name = f'{name}_{surname}'
+    else:
+        full_name = f'{name} {surname}'
+
+    return full_name
+
+
+def _print_name(name: str, surname: str, snake_case: bool):
+    full_name = _format_name(name, surname, snake_case=snake_case)
+    print(full_name)
